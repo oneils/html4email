29527c218fb7a8759899ea4e437f0e2fada570a1
Aliaksei Bahdanau <lex.nox@gmail.com>
2015-12-30 15:48:45 +0300

    #12: Add functionality for dynamic logo displaying     -  Digest titel validator is added

---
+++ src/main/kotlin/info/devbug/digest/DigestValidator.kt
@@ -0,0 +1,20 @@
+package info.devbug.digest
+
+import kotlin.text.Regex
+
+/**
+ * @author Aliaksei Bahdanau.
+ */
+class DigestValidator {
+    private val DIGEST_TITLE_PATTERN = "[A-Za-z0-9_ ]+[#]{1}[0-9]+"
+
+    /**
+     * Verifies is Digest has valid [title] or not.
+     *
+     * Returns true if title is valid, false otherwise.
+     */
+    fun isTitleValid(title: String): Boolean {
+        val regex = Regex(DIGEST_TITLE_PATTERN)
+        return regex.matches(title)
+    }
+}
---
+++ src/test/kotlin/info/devbug/digest/DigestValidatorSpec.groovy
@@ -0,0 +1,51 @@
+package info.devbug.digest
+
+import groovy.transform.TypeChecked
+import spock.lang.Specification
+
+/**
+ * @author Aliaksei Bahdanau.
+ */
+@TypeChecked
+class DigestValidatorSpec extends Specification {
+  DigestValidator validator
+
+  def setup() {
+    validator = new DigestValidator()
+  }
+  def "IsTitleValid should return true for valid input"() {
+    given:
+    def title = "BackEnd digest #12"
+
+    expect:
+    def result = validator.isTitleValid(title)
+    result
+  }
+
+  def "IsTitleValid should return false for double ##"() {
+    given:
+    def title = "BackEnd digest ##12"
+
+    expect:
+    def result = validator.isTitleValid(title)
+    !result
+  }
+
+  def "IsTitleValid should return false for more then one #"() {
+    given:
+    def title = "BackEnd # digest #12"
+
+    expect:
+    def result = validator.isTitleValid(title)
+    !result
+  }
+
+  def "IsTitleValid should return false for empty input"() {
+    given:
+    def title = ""
+
+    expect:
+    def result = validator.isTitleValid(title)
+    !result
+  }
+}
