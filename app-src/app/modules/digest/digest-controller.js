4014de60117768e69c073f5758c4407de08b3f82
Aliaksei Bahdanau <lex.nox@gmail.com>
2016-02-12 22:58:07 +0300

    useless console messages are removed

--- app-src/app/modules/digest/digest-controller.js
+++ app-src/app/modules/digest/digest-controller.js
@@ -1,5 +1,4 @@
 digestControllers.controller('DigestCtrl', ['$scope', '$http', function ($scope, $http ) {
-    console.log("Digest controller");
     var digest = {};
     digest.title = "BackEnd Digest #";
     $scope.digest = digest;
@@ -10,8 +9,6 @@ digestControllers.controller('DigestCtrl', ['$scope', '$http', function ($scope,
     $scope.createDigest = function () {
         var digest = {};
         digest.title = $scope.digest.title;
-
-        console.log(digest);
 
         $scope.digests.push(digest);
 
