64fd7263fc9a42c4e1f6ce8d8579636a191a2667
Alexey Bogdanov <lex.nox@gmail.com>
2016-02-12 10:45:04 +0300

    Archive page improved with Pager    - archive controller improved  to navigates through pages

--- app-src/app/modules/archive/archive-controller.js
+++ app-src/app/modules/archive/archive-controller.js
@@ -10,5 +10,17 @@ digestControllers.controller('ArchiveCtrl', ['$scope', '$http', function ($scope
     });
 
     $scope.$watch("totalItems", true);
+    $scope.$watch("digests", true);
 
+    $scope.$watch("currentPage", function (page) {
+        $http({
+            method: 'GET',
+            // on backend pages start from 0
+            url: '/v1/digests?page=' + (page - 1 )
+        }).then(function successCallback(response) {
+            $scope.digests = response.data.content;
+            $scope.totalItems = response.data.totalElements;
+        });
+
+    }, true);
 }]);
