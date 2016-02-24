1cd5bf9d039a2e7ba89bbedc9965c02a4a8c8f3e
Dzmitry Misiuk <minsler.by@gmail.com>
2016-02-25 00:43:21 +0300

    Added redux #19

--- app-src/app/react/components/feedback.jsx
+++ app-src/app/react/components/feedback.jsx
@@ -1,12 +1,15 @@
 import React, { Component, PropTypes } from 'react'
 
 export class Feedback extends Component {
+
     render() {
+        const { value } = this.props;
         return (
             <form role="form" name="feedbackForm">
                 <div className="form-group">
                     <label htmlFor="title">Title:</label>
-                    <input type="text" className="form-control" id="title" name="title" required/>
+                    <input type="text" className="form-control" id="title" name="title" required
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="username">Username:</label>
@@ -18,12 +21,17 @@ export class Feedback extends Component {
                 </div>
                 <div className="form-group">
                     <label htmlFor="content">Content:</label>
-                    <textArea rows="6" className="form-control" id="content" name="content"></textArea>
+                    <textArea rows="6" className="form-control" id="content"></textArea>
 
                 </div>
-                <button type="button" className="btn btn-default">Send</button>
+                <button type="button" className="btn btn-default" onclick>Send</button>
             </form>
         );
     }
 }
+
+Feedback.propTypes = {
+    value: PropTypes.object.isRequired
+};
+
 
--- app-src/app/react/core.jsx
+++ app-src/app/react/core.jsx
@@ -1,16 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import createLogger from 'redux-logger';
 import {List} from 'immutable';
-import {Feedback} from './feedback';
+import {Feedback} from './components/feedback';
 
-let myReactApp = {};
 
+const logger = createLogger();
+
+
+var feedbackNumber = 0;
+
+const INITIAL_FEEDBACK_FORM = {
+    feedback: {
+        id: 0, title: "Вопрос", username: "", email: "", content: "", status: "DRAFT"
+    },
+    feedbackList: new List()
+};
+
+
+function reducer(state = INITIAL_FEEDBACK_FORM, action) {
+    switch (action.type) {
+        case 'SEND_FEEDBACK':
+            let sendingFeedback = Object.assign({}, state, {id: feedbackNumber++, status: "SENDING"});
+            return {
+                feedback: INITIAL_FEEDBACK_FORM,
+                feedbackList: state.feedbackList.concat(List.of(sendingFeedback), state.feedbackList)
+            };
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(logger));
 
 function render() {
     ReactDOM.render(
-        < Feedback />,
+        < Feedback
+            value={store.getState().feedback}
+           />,
+
         document.getElementById('root')
     );
 }
 
 render();
+
+store.subscribe(render);
