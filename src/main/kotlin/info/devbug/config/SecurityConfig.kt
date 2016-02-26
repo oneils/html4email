6ea542ffad10b870b282321d5302cfe2b6604dd0
Aliaksei Bahdanau <lex.nox@gmail.com>
2016-02-26 17:20:52 +0300

    Security config is added.

--- src/main/kotlin/info/devbug/config/SecurityConfig.kt
+++ src/main/kotlin/info/devbug/config/SecurityConfig.kt
@@ -7,14 +7,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.oauth2.client.OAuth2ClientContext
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client
 
 /**
  * @author Aliaksei Bahdanau
  */
 @Configuration
 @EnableWebSecurity
-open class SecurityConfig() : WebSecurityConfigurerAdapter() {
+@EnableOAuth2Client
+@EnableAuthorizationServer
+open class SecurityConfig : WebSecurityConfigurerAdapter() {
 
+    @Autowired private var oauth2ClientContext: OAuth2ClientContext? = null
     @Autowired private var userDetailsService: UserDetailsService? = null
 
     override fun configure(auth: AuthenticationManagerBuilder) {
