3a3d4c29b40d8061c9fc73d4b72dafdcfe650f82
Aliaksei Bahdanau <lex.nox@gmail.com>
2016-03-02 19:43:57 +0300

    #21: Implement Authorization and Authentication functionality    - UI functionality for hiding menus for non Admins is added.

--- app-src/app/modules/controllers.js
+++ app-src/app/modules/controllers.js
@@ -7,6 +7,9 @@ digestControllers.controller('AuthCtrl', ['$scope', '$http', '$location', functi
     $http.get("/v1/user").success(function(data) {
         $scope.user = data.name;
         $scope.authenticated = true;
+        $scope.principal = data.principal;
+
+        $scope.isDisplayed = isDisplayed();
     }).error(function() {
         $scope.user = "N/A";
         $scope.authenticated = false;
@@ -22,6 +25,18 @@ digestControllers.controller('AuthCtrl', ['$scope', '$http', '$location', functi
             self.authenticated = false;
         });
     };
+
+    var isDisplayed = function () {
+        var authorities = $scope.principal.authorities;
+
+        for (var i = 0; i < authorities.length; i++) {
+            if (authorities[i].authority === "ROLE_ADMIN") {
+                return true;
+            }
+        }
+        return false;
+    };
+
 }]);
 
 
